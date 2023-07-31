8cd1f6f4c44f6e878fb28e34f5767bd450999bad
EduardoCasarotti <eduardocasarottiii@gmail.com>
2023-07-31 16:11:34 -0300

    avancando

---
+++ gitfind/src/components/itemList/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css';
+
+export default function ItemList({title, description}) {
+  return (
+    <div className='itemList'>
+        <strong>{title}</strong>
+        <p>{description}</p>
+        <hr/>
+    </div>
+  )
+}
--- gitfind/src/pages/Home/index.js
+++ gitfind/src/pages/Home/index.js
@@ -1,35 +1,45 @@
-import {Header} from '../../components/Header';
-import backgroud from '../../asssets/background.png';
-import './styles.css';
-import perfil from '../../asssets/perfil.png'
+import { useState } from "react";
+import { Header } from "../../components/Header";
+import backgroud from "../../asssets/background.png";
+import "./styles.css";
+import perfil from "../../asssets/perfil.png";
+import ItemList from "../../components/itemList";
 function App() {
+  const [user, setUser] = useState('');
+  const [currentUser, setCurrentUser] = useState(null);
+  const [repos, setRepos] =useState(null);
+  
   return (
     <div className="App">
-     <Header/>
-     <div className='conteudo'>
-      <img src={backgroud} className='background' alt='backgroud img'/>  
-        <div className='info'>
+      <Header />
+      <div className="conteudo">
+        <img src={backgroud} className="background" alt="backgroud img" />
+        <div className="info">
           <div>
-            <input name='usuario' placeholder='@username'/>
+            <input name="usuario" value={user} onChange={event =>setUser(event.target.value)} placeholder="@username" />
             <button>Buscar</button>
           </div>
-          <div className='perfil'>
-            <img src={perfil} className="profile"/>
+          <div className="perfil">
+            <img src={perfil} className="profile" />
             <div>
-              <h3>
-                Eduardo Alves Casarotti
-              </h3>
+              <h3>Eduardo Alves Casarotti</h3>
               <span>@EduardoCasarotti</span>
               <p>
                 Técnico em Desenvolvimento de Sistemas
-                <br/> 
+                <br />
                 Futuro desenvedor Front-end, 26 anos, São Paulo.
               </p>
             </div>
           </div>
-          <hr/>
+          <hr />
+          <div className=''>
+            <h4 className="repositorio">Repositórios</h4>
+            <ItemList title='teste1' description='teste de desicrição'/>
+            <ItemList title='teste1' description='teste de desicrição'/>
+            <ItemList title='teste1' description='teste de desicrição'/>
+          </div>
         </div>
-     </div>
+      </div>
     </div>
   );
 }
